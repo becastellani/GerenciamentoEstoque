c36ff3bda3e902bde71dfe236e7399e874ea2b49
Bernardo Castellani <becastellani10@gmail.com>
2023-07-28 16:56:48 -0300

    popup

--- app/cadastro-estoque.php
+++ app/cadastro-estoque.php
@@ -3,15 +3,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $quantidade = $_POST["quantidade"];
     $preco = $_POST["preco"];
-    $tipoId = $_POST["tipo"];
+    $tipoID = $_POST["tipo"];
 
     require_once '../app/Estoque.php';
     require_once '../config/config.php';
 
+    if (empty($nome) || empty($quantidade) || empty($preco) || empty($tipoID)) {
+        echo "Por favor, preencha todos os campos do formulário.";
+      } else {
+        echo "Formulário enviado com sucesso!";
+      }
 
     $estoque = new Estoque();
 
-    $estoque->addItem($nome, $quantidade, $preco, $tipoId);
+    $estoque->addItem($nome, $quantidade, $preco, $tipoID);
 
     header("Location: ../public/index.php");
     exit();
--- public/index.php
+++ public/index.php
@@ -5,24 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/index.css">
+
+    <script src="../node_modules/jquery/dist/jquery.js"></script> 
+    <script src="../node_modules/jquery-toast-plugin/dist/jquery.toast.min.js"></script>
+    <link rel="stylesheet" href="../node_modules/jquery-toast-plugin//dist/jquery.toast.min.css">
     <title>Cadastro de itens</title>
 </head>
 <body>
     <div class="container">
             <h2>Cadastrar Itens</h2>
 
-                <form action="../app/cadastro-estoque.php" method="post" class="form">
+                <form action="../app/cadastro-estoque.php" method="post" class="form" id="FormularioCadastro">
                 <label for="nome">Nome:</label>
-                <input class="input" type="text" name="nome" id="nome" required>
+                <input class="input" type="text" name="nome" id="nome">
 
                 <label for="quantidade">Quantidade:</label>
-                <input class="input" type="number" name="quantidade" id="quantidade" required>
+                <input class="input" type="number" name="quantidade" id="quantidade">
 
                 <label for="preco">Preço:</label>
-                <input class="input" type="number" step="0.01" name="preco" id="preco" required>
+                <input class="input" type="number" step="0.01" name="preco" id="preco">
 
                 <label for="tipo">Tipo:</label>
-                <select name="tipo" id="tipo" required>
+                <select name="tipo" id="tipo">
                     <option value="">Selecione o tipo</option>
                     <option value="1">Alimento</option>
                     <option value="2">Ferramentas</option>
@@ -37,7 +41,42 @@
     </div>
 
 
+    <script>
+$(document).ready(function() {
+  // Evento para lidar com o envio do formulário
+  $('form').submit(function(e) {
+    e.preventDefault(); // Previne o envio normal do formulário
 
+    // Validar se todos os campos estão preenchidos
+    var name = $('#nome').val();
+    var quantity = $('#quantidade').val();
+    var price = $('#preco').val();
+    var option = $('#tipo').val();
+
+    if (name === '' || quantity === '' || price === '' || option === '') {
+      // Exibir mensagem de erro com o toast
+      $.toast({
+        heading: 'Erro',
+        text: 'Por favor, preencha todos os campos!',
+        icon: 'error',
+        showHideTransition: 'fade',
+        position: 'top-right'
+      });
+    } else {
+      // Se todos os campos estiverem preenchidos, você pode enviar o formulário aqui, se necessário
+      // Exemplo: $(this).submit();
+      // Ou pode exibir uma mensagem de sucesso com o toast
+      $.toast({
+        heading: 'Sucesso',
+        text: 'Formulário enviado com sucesso!',
+        icon: 'success',
+        showHideTransition: 'slide',
+        position: 'top-right'
+      });
+    }
+  });
+});
+</script>
 
 </body>
 </html>
